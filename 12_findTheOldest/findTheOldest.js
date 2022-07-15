36a9c0f48f2d277670b4fff43f79fe62d358211c
angel <108492882+angel-472@users.noreply.github.com>
2022-07-15 12:34:46 -0400

    Complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldest;
+  let oldestAge = 0;
+  people.forEach(person => {
+    let age = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+    if(age > oldestAge){
+      oldestAge = age;
+      oldest = person;
+    }
+  });
+  return oldest;
 };
 
 // Do not edit below this line
