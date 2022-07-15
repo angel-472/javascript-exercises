87987a0f12d487de4a441c4f0a13e520db78b270
angel <108492882+angel-472@users.noreply.github.com>
2022-07-15 12:20:40 -0400

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  let last = 0;
+  let current = 1;
+  let next = 1;
+  for(i = 1; i < num; i++){
+    next = last + current;
+    last = current;
+    current = next;
+  }
+  return next;
 };
 
 // Do not edit below this line
