b405db48b8f4aee089c9718a16a80d1c363491c7
angel <108492882+angel-472@users.noreply.github.com>
2022-07-15 10:38:50 -0400

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let output = 0;
+	array.forEach(value => output += value);
+	return output;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let output = 1;
+	array.forEach(value => output *= value);
+	return output;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num < 0) return -1;
+	if(num == 0) return 1;
+	return (num * factorial(num - 1));
 };
 
 // Do not edit below this line
