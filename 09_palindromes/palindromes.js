42725a218f942f05b48eefd3fca3a9562fe06ff3
angel <108492882+angel-472@users.noreply.github.com>
2022-07-15 10:58:05 -0400

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  let regex = /[^a-zA-Z]+/g;
+  let filteredInput = input.replaceAll(regex,'').toLowerCase();
+  return reverseString(filteredInput) === filteredInput;
+};
 
+function reverseString(input) {
+  let array = input.split("");
+  return array.reverse().join("");
 };
 
 // Do not edit below this line
